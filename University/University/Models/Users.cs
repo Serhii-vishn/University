e15ef356d3b58b7098a88210936d515b3e497b1d
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-02-17 23:37:49 +0200

    added models

---
+++ University/University/Models/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace University.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Access { get; set; }
+    }
+}
