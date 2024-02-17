d31b4d5333cf0207c928e04e23babc1b98f5b5f9
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-02-17 23:45:35 +0200

    added UserTypeConfig

---
+++ University/University/Models/ModelsConfiguration/UsersTypeConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Models.ModelsConfiguration
+{
+    internal class UsersTypeConfiguration : IEntityTypeConfiguration<Users>
+    {
+        public void Configure(EntityTypeBuilder<Users> builder)
+        {
+            builder.ToTable("Users");
+
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.Id)
+                .UseIdentityColumn()
+                .IsRequired();
+
+            builder.Property(u => u.Login)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(u => u.Password)
+                .HasMaxLength(25)
+                .IsFixedLength()
+                .IsRequired();
+        }
+    }
+}
