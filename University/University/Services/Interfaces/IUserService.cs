a6338ef04fb8037deec97425c9e847278347dba1
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-02-21 16:55:10 +0200

    Update service user

---
+++ University/University/Services/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using University.Models;
+
+namespace University.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<User?> GetUserByIdAsync(int id);
+        Task<User?> GetUserByLogPassAsync(string login, string password);
+        Task<IList<User>> ListAsync();
+        Task<int> AddAsync(User user);
+        Task<int> UpdateAsync(User user);
+        Task<int> DeleteAsync(int id);
+    }
+}
--- University/University/Services/UserService.cs
+++ University/University/Services/UserService.cs
@@ -1,6 +1,46 @@
-﻿namespace University.Services
+﻿using University.Models;
+using University.Repositories.Interfaces;
+using University.Services.Interfaces;
+
+namespace University.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User?> GetUserByIdAsync(int id)
+        {
+            return await _userRepository.GetUserByIdAsync(id);
+        }
+
+        public async Task<User?> GetUserByLogPassAsync(string login, string password)
+        {
+            return await _userRepository.GetUserByLogPassAsync(login, password);
+        }
+
+        public Task<IList<User>> ListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> AddAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
