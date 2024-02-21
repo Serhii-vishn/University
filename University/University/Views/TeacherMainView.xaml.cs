e9670964466b7b73e3bb5378718d9577298c68bf
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-02-21 23:36:46 +0200

    update UI and added styles

--- University/University/Views/TeacherMainView.xaml.cs
+++ University/University/Views/TeacherMainView.xaml.cs
@@ -1,27 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace University.Views
 {
-    /// <summary>
-    /// Interaction logic for TeacherMainView.xaml
-    /// </summary>
     public partial class TeacherMainView : Window
     {
         public TeacherMainView()
         {
             InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
