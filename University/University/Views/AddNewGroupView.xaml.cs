9544d8d6a0ec0c976b57d1b6ef65a312cb57e602
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-02-28 22:18:59 +0200

    implement addNewGroupManu and methods

--- University/University/Views/AddNewGroupView.xaml.cs
+++ University/University/Views/AddNewGroupView.xaml.cs
@@ -1,27 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace University.Views
 {
-    /// <summary>
-    /// Interaction logic for AddNewGroupView.xaml
-    /// </summary>
     public partial class AddNewGroupView : Window
     {
         public AddNewGroupView()
         {
             InitializeComponent();
+            DataContext = new ViewModels.AddNewGroupVM();
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
