6fe273fa37102b93089f4d9a51a039ba5545243d
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-02-29 16:27:37 +0200

    update and add get full info teacher

--- University/University/Repositories/Interfaces/ITeacherRepository.cs
+++ University/University/Repositories/Interfaces/ITeacherRepository.cs
@@ -6,6 +6,7 @@ namespace University.Repositories.Interfaces
     {
         Task<Teacher?> GetAsync(int id);
         Task<IList<Teacher>> ListAsync();
+        Task<IList<Teacher>> GetAllAsync();
         Task<int> AddAsync(Teacher teacher);
         Task<int> UpdateAsync(Teacher teacher);
         Task<int> DeleteAsync(int id);
