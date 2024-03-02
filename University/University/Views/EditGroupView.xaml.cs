759614f8a52f385927407e206eb0e466598f8d35
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-03-02 23:45:38 +0200

    refactoring editDelete window

---
+++ University/University/Views/EditGroupView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using University.Models;
+using System.Windows;
+
+namespace University.Views
+{
+    public partial class EditGroupView : Window
+    {
+        public EditGroupView(Group group)
+        {
+            InitializeComponent();
+            MessageBox.Show(group.GroupName);
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
