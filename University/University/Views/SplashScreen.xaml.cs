7a23927e007c611fd30829530581a3e7b709c349
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-03-18 15:47:05 +0200

    added splashScreen

---
+++ University/University/Views/SplashScreen.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Threading;
+using System.Windows;
+using University.View;
+
+namespace University.Views
+{
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            var worker = new BackgroundWorker();
+
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+            worker.RunWorkerAsync();
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                (sender as BackgroundWorker).ReportProgress(i);
+                Thread.Sleep(50);
+            }
+        }
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+
+            if(progressBar.Value == 50) 
+            {
+                var taskWindow = new LoginView();
+                taskWindow.Closed += (s, eventArgs) =>
+                {
+                    taskWindow = null;
+                };
+
+                taskWindow.Show();
+            }
+        }
+    }
+}
