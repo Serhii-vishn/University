f4cc4f4a5116d032d61072bb6c9a769b63663524
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-03-26 00:04:24 +0200

    implement servise review

---
+++ University/University/Services/Interfaces/IReviewService.cs
@@ -0,0 +1,11 @@
+﻿namespace University.Services.Interfaces
+{
+    public interface IReviewService
+    {
+        Task<Review?> GetReviewByIdAsync(int id);
+        Task<IList<Review>> ListAsync();
+        Task<int> AddAsync(Review review);
+        Task<int> UpdateAsync(Review review);
+        Task<int> DeleteAsync(int id);
+    }
+}
---
+++ University/University/Services/ReviewService.cs
@@ -0,0 +1,37 @@
+﻿namespace University.Services
+{
+    public class ReviewService : IReviewService
+    {
+        private readonly IReviewRepository _reviewRepository;
+
+        public ReviewService(IReviewRepository reviewRepository)
+        {
+            _reviewRepository = reviewRepository;
+        }
+
+        public Task<Review?> GetReviewByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Review>> ListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> AddAsync(Review review)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(Review review)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
