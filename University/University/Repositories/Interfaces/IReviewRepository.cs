30191073ee6832914c5977b3f0fb6a0a794d549e
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-03-25 23:57:19 +0200

    added repository for Review

---
+++ University/University/Repositories/Interfaces/IReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace University.Repositories.Interfaces
+{
+    public interface IReviewRepository
+    {
+        Task<Review?> GetAsync(int id);
+        Task<Review?> GetAllAsync(int id);
+        Task<IList<Review>> ListAsync();
+        Task<IList<Review>> ListAllAsync();
+        Task<int> AddAsync(Review review);
+        Task<int> UpdateAsync(Review review);
+        Task<int> DeleteAsync(int id);
+    }
+}
---
+++ University/University/Repositories/ReviewRepository.cs
@@ -0,0 +1,100 @@
+﻿
+using University.DbContexts;
+
+namespace University.Repositories
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public ReviewRepository(ApplicationDbContext applicationContext)
+        {
+            _applicationDbContext = applicationContext;
+        }
+
+        public async Task<Review?> GetAsync(int id)
+        {
+            return await _applicationDbContext.Reviews
+                .Where(r => (r.Id == id))
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task<Review?> GetAllAsync(int id)
+        {
+            return await _applicationDbContext.Reviews
+                .Where(r => (r.Id == id))
+                .Include(r => r.Student)
+                .Include(r => r.Teacher)
+                .SingleOrDefaultAsync();
+        }
+
+        public Task<IList<Review>> ListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IList<Review>> ListAllAsync()
+        {
+            return await _applicationDbContext.Reviews
+                .Include(r => r.Student)
+                .Include(r => r.Teacher)
+                .ToListAsync();
+        }
+
+        public async Task<int> AddAsync(Review review)
+        {
+            var transaction = await _applicationDbContext.Database.BeginTransactionAsync();
+
+            try
+            {
+                var result = await _applicationDbContext.Reviews.AddAsync(review);
+                await _applicationDbContext.SaveChangesAsync();
+                await transaction.CommitAsync();
+
+                return result.Entity.Id;
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+
+        public async Task<int> UpdateAsync(Review review)
+        {
+            var transaction = await _applicationDbContext.Database.BeginTransactionAsync();
+
+            try
+            {
+                _applicationDbContext.Entry(review).State = EntityState.Modified;
+                await _applicationDbContext.SaveChangesAsync();
+                await transaction.CommitAsync();
+
+                return review.Id;
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            var transaction = await _applicationDbContext.Database.BeginTransactionAsync();
+
+            try
+            {
+                _applicationDbContext.Entry((await GetAsync(id))!).State = EntityState.Deleted;
+                await _applicationDbContext.SaveChangesAsync();
+                await transaction.CommitAsync();
+                return id;
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+    }
+}
